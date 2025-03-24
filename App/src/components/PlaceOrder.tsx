1b873a99a7b2f1677920dc7cb8d3a97e41c9ae11
belunatic <abelluvyou@yahoo.com>
2025-03-24 15:16:40 -0700

    added the remove items from order

--- App/src/components/PlaceOrder.tsx
+++ App/src/components/PlaceOrder.tsx
@@ -3,19 +3,25 @@ import { MenuItem } from "../App.types";
 import { AppDataContext } from "../context/AppContext";
 
 const PlaceOrder: FC = () => {
-	const { currentOrder } = AppDataContext();
-
+	const { currentOrder, setCurrentOrder } = AppDataContext();
+	//keep track of the total price of the order
 	const [total, setTotal] = useState<number>(0);
-
+	//display the order
 	const order = currentOrder.map((item: MenuItem) => {
 		return (
 			<div key={item.id}>
 				<p>
-					{item.name} <span>{item.price}</span>
+					{item.name} <span>{item.price}</span>{" "}
+					<span onClick={() => removeItem(item.id)}>X</span>
 				</p>
 			</div>
 		);
 	});
+
+	//remove an item from the order
+	const removeItem = (id: number) => {
+		setCurrentOrder(currentOrder.filter((item) => item.id !== id));
+	};
 
 	useEffect(() => {
 		setTotal(currentOrder.reduce((acc, item) => acc + item.price, 0));
