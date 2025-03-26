8c4c2bbcd757045aa434b294f93d71e405417241
belunatic <abelluvyou@yahoo.com>
2025-03-26 14:17:26 -0700

    added a function for complete order, made the component display table

--- App/src/components/OrderQueue.tsx
+++ App/src/components/OrderQueue.tsx
@@ -1,42 +1,65 @@
 import { FC } from "react";
 import { AppDataContext } from "../context/AppContext";
-import { MenuItem } from "../App.types";
+import { MenuItem, Order } from "../App.types";
 
 const OrderQueue: FC = () => {
-	const { orderQueue } = AppDataContext();
+	const { orderQueue, setOrderQueue } = AppDataContext();
 
+	//when the order is complete, change it status
+	const completeOrder = (id: number) => {
+		const updatedOrderQueue: Order[] = orderQueue.map((order) =>
+			order.id === id ? { ...order, status: "complete" } : order
+		);
+		setOrderQueue([...updatedOrderQueue]);
+	};
+
+	//display orders that are not complete
 	const displayOrderQueue = orderQueue.map((order) => {
-		return (
-			<table>
-				<thead>
-					<tr>
-						<th>Order ID</th>
-						<th>Pizzas</th>
-						<th>Total</th>
-						<th>Status</th>
-					</tr>
-				</thead>
-				<tbody>
-					{orderQueue.map((order) => (
-						<tr key={order.id}>
-							<td>{order.id}</td>
-							<td>
-								{order.pizzas.map((pizza: MenuItem, index: number) => {
-									return order.pizzas.length === 1 ||
-										order.pizzas.length - 1 === index
-										? `${pizza.name} `
-										: `${pizza.name}, `;
-								})}
-							</td>
-							<td>{order.total}</td>
-							<td>{order.status}</td>
-						</tr>
-					))}
-				</tbody>
-			</table>
+		return order.status === "ordered" ? (
+			<tr key={order.id}>
+				<td>{order.id}</td>
+				<td>
+					{order.pizzas.map((pizza: MenuItem, index: number) => {
+						return order.pizzas.length === 1 ||
+							order.pizzas.length - 1 === index
+							? `${pizza.name} `
+							: `${pizza.name}, `;
+					})}
+				</td>
+				<td>{order.total}</td>
+				<td>
+					<input
+						type="checkbox"
+						checked={false}
+						onChange={() => completeOrder(order.id)}
+					/>
+					completed
+				</td>
+			</tr>
+		) : (
+			""
 		);
 	});
-	return <div>{displayOrderQueue ? displayOrderQueue : "No Order Places"}</div>;
+
+	return (
+		<div>
+			{displayOrderQueue ? (
+				<table>
+					<thead>
+						<tr>
+							<th>Order ID</th>
+							<th>Pizzas</th>
+							<th>Total</th>
+							<th>Status</th>
+						</tr>
+					</thead>
+					<tbody>{displayOrderQueue}</tbody>
+				</table>
+			) : (
+				"No Order Places"
+			)}
+		</div>
+	);
 };
 
 export default OrderQueue;
