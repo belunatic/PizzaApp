ed2be7e07a6e615258630bbe86b5aeda83431c6a
belunatic <abelluvyou@yahoo.com>
2025-03-27 16:54:01 -0700

    enhance Menu component layout and order placement logic

--- App/src/components/Menu.tsx
+++ App/src/components/Menu.tsx
@@ -6,7 +6,7 @@ const Menu: FC = () => {
   const { menu, setCurrentOrder } = AppDataContext();
 
   const placeOrder = (item: MenuItem): void => {
-    setCurrentOrder((prev) => [item, ...prev]);
+    setCurrentOrder((prev) => [...prev, item]);
   };
 
   const pizzaMenu = menu.map((item: MenuItem) => {
@@ -23,7 +23,7 @@ const Menu: FC = () => {
   });
 
   return (
-    <div className="mb-4 w-1/2">
+    <div className="mb-4 w-1/2 px-1">
       <h2 className="mb-2 border-b-2 text-center text-2xl uppercase">Menu</h2>
       <div className="grid grid-cols-4">{pizzaMenu}</div>
     </div>
