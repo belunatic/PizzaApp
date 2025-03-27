7d40724ac431dc139cb0e69fc63205004bb4bd6b
belunatic <abelluvyou@yahoo.com>
2025-03-27 15:38:03 -0700

    added classname for the menu component

--- App/src/components/Menu.tsx
+++ App/src/components/Menu.tsx
@@ -3,21 +3,30 @@ import { AppDataContext } from "../context/AppContext";
 import { MenuItem } from "../App.types";
 
 const Menu: FC = () => {
-	const { menu, setCurrentOrder } = AppDataContext();
+  const { menu, setCurrentOrder } = AppDataContext();
 
-	const placeOrder = (item: MenuItem): void => {
-		setCurrentOrder((prev) => [item, ...prev]);
-	};
+  const placeOrder = (item: MenuItem): void => {
+    setCurrentOrder((prev) => [item, ...prev]);
+  };
 
-	const pizzaMenu = menu.map((item: MenuItem) => {
-		return (
-			<div key={item.id}>
-				<button onClick={() => placeOrder(item)}>{item.name}</button>
-			</div>
-		);
-	});
+  const pizzaMenu = menu.map((item: MenuItem) => {
+    return (
+      <div key={item.id}>
+        <button
+          onClick={() => placeOrder(item)}
+          className="btn cursor-pointer rounded-sm bg-lime-600 px-4 py-8 text-center"
+        >
+          {item.name}
+        </button>
+      </div>
+    );
+  });
 
-	return <div>{pizzaMenu}</div>;
+  return (
+    <div className="w-1/2">
+      <div className="grid grid-cols-4">{pizzaMenu}</div>
+    </div>
+  );
 };
 
 export default Menu;
